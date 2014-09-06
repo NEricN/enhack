898a168724613b5cf22207c98a81954f6b7fbcb7
Eric Nguyen <neric@berkeley.edu>
2014-09-06 11:04:22 -0700

    Login/logout now works

---
+++ README.md
@@ -0,0 +1,4 @@
+enhack
+======
+
+Name TBA
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var nunjucks = require('nunjucks');
 var app = express();
+
+var routes = require('./routes');
 
 var uristring = "mongodb://admin:admin@ds035260.mongolab.com:35260/enhack";
 
@@ -12,12 +15,27 @@ mongoose.connect(uristring, function (err, res) {
   }
 });
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app
 })
+
+app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'nunjucks');
+app.use(function(req,res,next) {
+	res.locals.session = req.session;
+	next();
+})
+app.use(express.cookieParser('secret'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', routes.index);
+app.get('/oauth', routes.oauth);
+app.get('/oauth_callback', routes.oauth_callback);
+app.get('/clear', routes.clear);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
